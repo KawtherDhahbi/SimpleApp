e94fcb57c06812f9a1ec4093a16d1a5ff0904c25
KawtherDhahbi <kawtherdhahbi1@gmail.com>
2022-02-08 01:46:53 +0100

    sample commit

---
+++ app/src/main/java/com/iot2/simpleapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.iot2.simpleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView text1;
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text1 = (TextView) findViewById(R.id.text1);
+        String ch = text1.getText().toString();
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button is clicked", Toast.LENGTH_SHORT).show();
+                text1.setText("Bonjour");
+            }
+        });
+    }
+}
+
+
+
+
